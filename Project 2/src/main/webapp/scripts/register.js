42205b5abf2bb4b6a1ceb71767a228e3a5f665b4
cj-jones-codes <christopher.jones@revature.net>
2022-02-18 14:39:36 -0800

    Jeffrey Updated register.js

--- Project 2/src/main/webapp/scripts/register.js
+++ Project 2/src/main/webapp/scripts/register.js
@@ -1,23 +1,16 @@
 async function register() {
+    let url = "http://localhost:8080/users";
     let user = {
         username: document.getElementById("register_username").value,
         password: document.getElementById("register_password").value
-    };
-
-    console.log(JSON.stringify(user));
-      let response = await fetch("http://localhost:8080/users", {
+    }
+    let response = await fetch(url, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json;charset=utf-8'
+            'Content-type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify(user)
-      });
-      try {
-        let result = await response.json();
-        alert(result.message);
-      } catch (error) {
-        console.error(error);
-      }
-};
+    })
+}
 
 
