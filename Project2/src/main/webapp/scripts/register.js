34df5d460880b716171945a90c52cce7defb7f57
jlor993 <jlor993@gmail.com>
2022-02-21 13:49:14 -0600

    Register validation and redirect

--- Project2/src/main/webapp/scripts/register.js
+++ Project2/src/main/webapp/scripts/register.js
@@ -1,9 +1,26 @@
 async function register() {
+    let user_input = document.getElementById("register_username").value;
+    let pass_input = document.getElementById("register_password").value;
+
+    let err = document.getElementById("error_message");
+    if (user_input == "") {
+        err.style.visibility = "visible";
+        err.innerHTML = "Please enter a username.";
+        return false;
+    } else if(pass_input == "") {
+        err.style.visibility = "visible";
+        err.innerHTML = "Please enter a password.";
+        return false;
+    } else {
+        err.style.visibility = "hidden";
+    }
+
     let url = "http://localhost:8080/users";
     let user = {
-        username: document.getElementById("register_username").value,
-        password: document.getElementById("register_password").value
+        username: user_input,
+        password: pass_input
     }
+
     let response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -11,6 +28,16 @@ async function register() {
         },
         body: JSON.stringify(user)
     })
+    .then(function(response) {
+        //console.log(response.status);
+        if (!response.ok) {
+            err.style.visibility = "visible";
+            err.innerHTML = "Invalid username or password, or username already exists.";
+        } else {
+            location.href = 'login.html';
+        }
+    })
+
 }
 
 
