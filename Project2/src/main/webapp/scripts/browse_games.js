f8fe11ff39732f577f67eb8f2c82ff6c211d4dbb
jlor993 <jlor993@gmail.com>
2022-02-22 15:22:12 -0600

    Game list completed with redirects, backlog button tied to session

--- Project2/src/main/webapp/scripts/browse_games.js
+++ Project2/src/main/webapp/scripts/browse_games.js
@@ -5,11 +5,16 @@ async function gamePopulation() {
     .then((response) => response.json());
 
     for(let i = 0; i < response.length; i++) {
-
         let div = document.getElementById("games_list");
         let gameDiv = document.createElement("div");
+        let a = document.createElement("a");
+
+        a.href = "view_game.html?game_id=" + response[i].gameId;
+        a.innerHTML += "<img class=\"game_covers\" src=\"" + response[i].backgroundImage + "\">";
+        a.innerHTML += "<p class=\"game_title\">" + response[i].name + "</p>";
+
         gameDiv.className = "game_item";
-        gameDiv.innerHTML += "<img class=\"game_covers\" src=\"" + response[i].backgroundImage + "\">";
+        gameDiv.appendChild(a);
         div.appendChild(gameDiv);
     }
 }
--- Project2/src/main/webapp/scripts/session.js
+++ Project2/src/main/webapp/scripts/session.js
@@ -1,9 +1,11 @@
 function checkSession() {
     if(localStorage.getItem("currentUser")) {
+        document.getElementById("backlog").style.visibility = "visible";
         document.getElementById("logout").style.visibility = "visible";
         document.getElementById("login").style.visibility = "hidden";
         document.getElementById("register").style.visibility = "hidden";
     } else {
+        document.getElementById("backlog").style.visibility = "hidden";
         document.getElementById("logout").style.visibility = "hidden";
         document.getElementById("login").style.visibility = "visible";
         document.getElementById("register").style.visibility = "visible";
