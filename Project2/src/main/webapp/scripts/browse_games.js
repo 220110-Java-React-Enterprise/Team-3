a511e1dac2cdfd9373bf3a4731beac8230203efd
cj-jones-codes <christopher.jones@revature.net>
2022-02-22 07:23:23 -0800

    CJ updated GameController (path is now /browse_games) and create JavaScript for browse_games

--- Project2/src/main/webapp/scripts/browse_games.js
+++ Project2/src/main/webapp/scripts/browse_games.js
@@ -1,12 +1,15 @@
 async function gamePopulation() {
     let url = "http://localhost:8080/browse_games";
-    console.log("async");
     let response = await fetch(url)
     .then((response) => response.json())
     .then((games) => {
         return games;
     });
     for(let i = 0; i < response.length; i++) {
-        console.log(response[i]);
+        let gameI = {
+        gameName:response[i].name,
+        image:response[i].backgroundImage
+        }
+        console.log(JSON.stringify(gameI))
     }
 }
