ffcd61045fdc156736ffd498d1a7370c0dc1b4b7
jlor993 <jlor993@gmail.com>
2022-02-22 16:13:16 -0600

    Game details page

--- Project2/src/main/webapp/scripts/browse_games.js
+++ Project2/src/main/webapp/scripts/browse_games.js
@@ -2,7 +2,7 @@ async function gamePopulation() {
     let url = "http://localhost:8080/browse_games";
 
     let response = await fetch(url)
-    .then((response) => response.json());
+        .then((response) => response.json());
 
     for(let i = 0; i < response.length; i++) {
         let div = document.getElementById("games_list");
